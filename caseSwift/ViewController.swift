194108eb9e6aa81937ed3534c4ff25aa0711d846
Cihangir Sungur <cihangir.sungur@gmail.com>
2018-03-02 16:49:39 +0300

    Initial Commit

---
+++ caseSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  caseSwift
+//
+//  Created by Cihangir Sungur on 3/2/18.
+//  Copyright © 2018 Cihangir Sungur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
